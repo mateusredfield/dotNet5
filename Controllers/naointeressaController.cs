bb0752c67d5004b6b0894d68859587de1a44a848
mateusredfield <mateuscampari@gmail.com>
2022-09-20 08:46:28 -0300

    dificil fazer uma coisa simples em asp .net 5

---
+++ Controllers/naointeressaController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace qqnome.Controllers
+{
+    //[Route("api")]
+    [ApiController]
+    [Route("[controller]")]
+    public class naointeressaController : Controller
+    {
+        [HttpGet("{semcursodemoramais}/{osso}")]
+        //public IActionResult Index()
+        public IEnumerable<Cliente> naosei()
+        {
+            //return Ok(DateTime.Now.ToShortDateString());
+            //return View();
+            return Ranges();
+        }
+        //[HttpGet]
+        //public IEnumerable<int> Get()
+        //{
+        //    return Enumerable.Range(1,10);
+        //}
+        //public IActionResult Ranges()
+        [HttpGet("{osso}")]
+        public IEnumerable<Cliente> Ranges()
+        {
+            var clientes = new List<Cliente>
+                                {
+                                    new Cliente("fulano", 123),
+                                    new Cliente("beltrano", 456)
+                                };
+            //return Ok(Enumerable.Range(1, 10));
+            return clientes;
+        }
+    }
+    public class Cliente{
+        public string nome { get; set; }
+        public int cpf { get; set; }
+        public Cliente(string nome, int cpf) {
+            this.nome = nome;
+            this.cpf = cpf;
+        }
+
+    }
+}
